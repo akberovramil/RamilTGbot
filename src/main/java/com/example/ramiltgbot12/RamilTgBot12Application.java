1c2921069fa02e2ea2201313c8639ff864e657b6
akberovramil <ramil.akberov96@gmail.com>
2023-05-18 19:58:46 +0300

    1

---
+++ src/main/java/com/example/ramiltgbot12/RamilTgBot12Application.java
@@ -0,0 +1,13 @@
+package com.example.ramiltgbot12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamilTgBot12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamilTgBot12Application.class, args);
+    }
+
+}
